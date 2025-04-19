f71db6b803eb4c4a713f75603d58c33d19de6b50
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2025-04-19 18:56:32 -0500

    Build.bat now dynamically uses the latest build tools.

--- build.bat
+++ build.bat
@@ -1,22 +1,36 @@
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::Install TerrariaDepotDownloader Via MSBuild                    ::
+::Install TerrariaDepotDownloader Via MSBuild Tools              ::
 ::Gihub https://github.com/RussDev7/TerrariaDepotDownloader      ::
 ::Devoloped, Maintained, And Sponsored By Discord:dannyruss      ::
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @ECHO OFF
 
-Rem | Set Params
+Rem | Set parameters.
 Set "VersionPrefix=1.8.5.7"
 Set "filename=TerrariaDepotDownloader-%VersionPrefix%"
 
-Rem | Install SLN Under x64 Profile
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe ".\src\TerrariaDepotDownloader.sln" /p:Configuration=Release /p:Platform=x64"
+Rem | Put the expected location of vswhere into a variable.
+set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 
-Rem | Delete Paths & Create Paths
+Rem | Ask for the newest VS install that includes Microsoft.Component.MSBuild
+Rem | and let vswhere do the glob‑expansion that finds MSBuild.exe.
+for /f "usebackq tokens=*" %%I in (`
+  "%VSWHERE%" -latest ^
+              -products * ^
+              -requires Microsoft.Component.MSBuild ^
+              -find MSBuild\**\Bin\MSBuild.exe
+`) do (
+    set "MSBUILD=%%I"
+)
+
+Rem | Install SLN under x64 profile.
+"%MSBUILD%" ".\src\TerrariaDepotDownloader.sln" /p:Configuration=Release /p:Platform=x64"
+
+Rem | Delete paths & create paths.
 rmdir /s /q ".\release"
 mkdir ".\release"
 
-Rem | Copy Over Items
+Rem | Copy over items.
 xcopy /E /Y ".\src\TerrariaDepotDownloader\bin\x64\Release" ".\release\%filename%\"
 copy ".\src\TerrariaDepotDownloader\ExternalResources\ManifestVersions.cfg" ".\release\%filename%\"
 copy ".\src\packages\DotNetZip.1.16.0\lib\net40\DotNetZip.dll" ".\release\%filename%\"
@@ -29,15 +43,15 @@ copy ".\src\TerrariaDepotDownloader\DepotDownloader\DepotDownloader.deps.json" "
 copy ".\src\TerrariaDepotDownloader\DepotDownloader\System.IO.Hashing.dll" ".\release\%filename%\"
 copy ".\src\TerrariaDepotDownloader\DepotDownloader\QRCoder.dll" ".\release\%filename%\"
 
-Rem | Clean Up Files
+Rem | Clean up files.
 del /f /q /s ".\release\*.xml"
 del /f /q /s ".\release\*.pdb"
 del /f /q /s ".\release\*.config"
 
-Rem | Delete & Create ZIP Release
+Rem | Delete & create ZIP release.
 if exist ".\%filename%.zip" (del /f ".\%filename%.zip")
 powershell.exe -nologo -noprofile -command "Compress-Archive -Path ".\release\*" -DestinationPath ".\%filename%.zip""
 
-Rem | Operation Complete
+Rem | Operation complete.
 echo(
 pause
