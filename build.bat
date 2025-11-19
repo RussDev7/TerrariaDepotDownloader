2cdd725399f2a166b2e0f076ae614f2cea4f2c37
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2025-11-19 12:25:21 -0600

    Bumped version.

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@
 @ECHO OFF
 
 Rem | Set parameters.
-Set "VersionPrefix=1.8.5.7"
+Set "VersionPrefix=1.8.5.8"
 Set "filename=TerrariaDepotDownloader-%VersionPrefix%"
 
 Rem | Put the expected location of vswhere into a variable.
@@ -55,3 +55,4 @@ powershell.exe -nologo -noprofile -command "Compress-Archive -Path ".\release\*"
 Rem | Operation complete.
 echo(
 pause
+
